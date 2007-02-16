fafe5be0c68660d58aab02fc693aca2cbf76b9f6
krab <krab@931d27ba-d1ef-0310-baf8-e73a6a934ec9>
2007-02-16 13:23:46 +0000

    remove printf debugs
    git-svn-id: https://svn.trifork.com/svn/trifork/projects/hotruby@66 931d27ba-d1ef-0310-baf8-e73a6a934ec9

--- modules/vm-shared/ruby/2.0/test/unit/util/backtracefilter.rb
+++ modules/vm-shared/ruby/2.0/test/unit/util/backtracefilter.rb
@@ -2,9 +2,9 @@ module Test
   module Unit
     module Util
       module BacktraceFilter
-        p TESTUNIT_FILE_SEPARATORS = %r{[\\/:]}
-        p TESTUNIT_PREFIX = __FILE__.split(TESTUNIT_FILE_SEPARATORS)[0..-3]
-        p TESTUNIT_RB_FILE = /\.rb\Z/
+##        p TESTUNIT_FILE_SEPARATORS = %r{[\\/:]}
+##        p TESTUNIT_PREFIX = __FILE__.split(TESTUNIT_FILE_SEPARATORS)[0..-3]
+##        p TESTUNIT_RB_FILE = /\.rb\Z/
 
         def filter_backtrace(backtrace, prefix=nil)
           return ["No backtrace"] unless(backtrace)
@@ -13,15 +13,15 @@ module Test
           else
             TESTUNIT_PREFIX
           end
-            p "split_p:#{split_p}"
+##            p "split_p:#{split_p}"
           match = proc do |e|
-            p "E:#{e}"
+##            p "E:#{e}"
             split_e = e.split(TESTUNIT_FILE_SEPARATORS)[0, split_p.size]
-            p "split_e:#{e.split(TESTUNIT_FILE_SEPARATORS)}"
-            p "split_p.size = #{split_p.size}"
-            p "split_e:#{e.split(TESTUNIT_FILE_SEPARATORS)[0, split_p.size]}"
+##            p "split_e:#{e.split(TESTUNIT_FILE_SEPARATORS)}"
+##            p "split_p.size = #{split_p.size}"
+##            p "split_e:#{e.split(TESTUNIT_FILE_SEPARATORS)[0, split_p.size]}"
             next false unless(split_e[0..-2] == split_p[0..-2])
-            p "split_e[-1]:#{split_e[-1]}"
+##            p "split_e[-1]:#{split_e[-1]}"
             split_e[-1].sub(TESTUNIT_RB_FILE, '') == split_p[-1]
           end
           return backtrace unless(backtrace.detect(&match))
