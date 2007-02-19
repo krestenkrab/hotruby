b02ced6286409938b96c28bbbac2154641af029f
ofo <ofo@931d27ba-d1ef-0310-baf8-e73a6a934ec9>
2007-02-19 20:03:35 +0000

    Bare lidt oprydning.
    git-svn-id: https://svn.trifork.com/svn/trifork/projects/hotruby@70 931d27ba-d1ef-0310-baf8-e73a6a934ec9

--- modules/vm-shared/src/com/trifork/hotruby/interp/LexicalBindings.java
+++ modules/vm-shared/src/com/trifork/hotruby/interp/LexicalBindings.java
@@ -6,12 +6,10 @@ import com.trifork.hotruby.runtime.Global;
 import com.trifork.hotruby.runtime.MetaModule;
 import com.trifork.hotruby.runtime.RubyMethod;
 import com.trifork.hotruby.runtime.RubyMethodAccessor;
-import com.trifork.hotruby.runtime.RubyRuntime;
 import com.trifork.hotruby.runtime.Selector;
 import com.trifork.hotruby.runtime.ThreadState.ModuleFrame;
 
 public class LexicalBindings implements CallContext {
-	
 	public Global[] globals;
 	public ConstantAccessor[] constants;
 
@@ -28,8 +26,7 @@ public class LexicalBindings implements CallContext {
 		this.frame = frame;
 		globals = iseq.getGlobals(lexical_context.getRuntime());
 		constants = iseq.getConstants(lexical_context, frame);
-		selectors = prepare_selectors(iseq.getRuntime(), 
-				iseq.getSelectors());
+		selectors = prepare_selectors(iseq.getSelectors());
 		super_method = iseq.getSuperMethod(lexical_context, self_is_module);
 	}
 	
@@ -37,8 +34,7 @@ public class LexicalBindings implements CallContext {
 		return frame;
 	}
 
-
-	private Selector[] prepare_selectors(RubyRuntime runtime, String[] names) {
+	private Selector[] prepare_selectors(String[] names) {
 		Selector[] result = new Selector[names.length];
 		for (int i = 0; i < names.length; i++) {
 			result[i] = lexical_context.getSelector(names[i]);
@@ -46,14 +42,11 @@ public class LexicalBindings implements CallContext {
 		return result;
 	}
 
-
 	public MetaModule getCaller() {
 		return lexical_context;
 	}
 
-
 	public RubyMethod get_super_method() {
 		return this.super_method.get();
 	}
-
 }
